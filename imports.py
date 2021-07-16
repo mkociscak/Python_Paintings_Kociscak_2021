43d1288a14197c08381cc196aab4d812e12f9a22
Megan Kociscak <megankociscak@Megans-MBP.attlocal.net>
2021-07-15 23:00:41 -0400

    show user directions

--- imports.py
+++ imports.py
@@ -2,10 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import urllib
-#import datetime
-#import csv
-#print(soup.prettify())
-#print("What article would you like to see?")
+
+
 
 URL = "https://www.megankociscak.com"
 r = requests.get(URL)
@@ -17,6 +15,18 @@ for div in divs:
     print(div.text)
 
 
+def show_directions():
+    print("Which title would you like to see?")
+    print("Type NONE to exit")
+
+
+show_directions()
+while True:
+    new_choice = input("> ")
+    #open new webpage and repeat prompt
+    show_directions()
+    if new_choice == "NONE":
+        break
 
 
 
