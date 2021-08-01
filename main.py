4a341ffffa6062c3f2c366e06b0505b0535e7520
Megan Kociscak <megankociscak@Megans-MacBook-Pro.local>
2021-07-31 19:17:31 -0500

    additions to lists

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-#import re
+import re
 import urllib
 import sys
 import pandas as pd
@@ -28,13 +28,10 @@ for div in divs:
 #df = pd.DataFrame(a)
 #df.to_csv(r'CSV_files/property_info.csv')
 
-
-
 print()
 print()
 
-#MASTER_EXIT = "EXIT"
-#MASTER_LIST = "LIST"
+MASTER_EXIT = "EXIT"
 
 
 my_list = ["Blood E Surplus : How's Your Vision? No. 1",
@@ -42,59 +39,89 @@ my_list = ["Blood E Surplus : How's Your Vision? No. 1",
 "Brain Set No. 1",
 "Brain Set No. 2",
 "Reevaluate Reevaluating",
-"Reevaluate Your Craft"]
+"Reevaluate Your Craft",
+"Darlin', Your Head's Not Right",
+"Oh Shun View: I Had to Make him Leave",
+"A Pansy-Ass Box Of Whoop-Ass",
+"One Day, Baby, We'll Be Gold",
+"A Parking Lot Made Up Of Makeup (Another Waste Of Space)"]
 
 def show_directions():
-    print("Which title would you like to see next?")
+    #print("Which title would you like to see next?")
     print("Type LIST to view available titles")
     print("Type EXIT to exit")
+    #new_choice = input("Which title would you like to see? (copy/paste from list)  ")
 
 new_choice = input("Which title would you like to see? (copy/paste from list)  ")  #choose from scraped list and repeat prompt
 #while new_choice != "True":
-    #print(input("Which title would you like to see?  "))
+#print(input("Which title would you like to see?  "))
 #def new_choice_two = input("Which title would you like to see next?  ")
+#while True:  
 
-
-#while True:   
+ 
 if new_choice == "Blood E Surplus : How's Your Vision? No. 1":
         print("https://megankociscak.com/#/blood-e-surplus/" )
         print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
 
-elif new_choice == "Nothing is Ever Really Black and White": 
+if new_choice == "Nothing is Ever Really Black and White": 
         print("https://megankociscak.com/#/nothing-is-ever-really-black-and-white/")
-        print("Which title would you like to see next?  ")
-        print("Type LIST to view available titles")
-        print("Type EXIT to exit")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
 
-elif new_choice == "Brain Set No. 1": 
+if new_choice == "Brain Set No. 1": 
         print("https://megankociscak.com/#/brain-set-no-1/")
-        print("Which title would you like to see next?  ")
-        print("Type LIST to view available titles")
-        print("Type EXIT to exit")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
 
-elif new_choice == "Brain Set No. 2": 
+if new_choice == "Brain Set No. 2": 
         print("https://megankociscak.com/#/brain-set-no-2/")
-        print("Which title would you like to see next?  ")
-        print("Type LIST to view available titles")
-        print("Type EXIT to exit")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
 
-elif new_choice == "Reevaluate Reevaluating": 
+if new_choice == "Reevaluate Reevaluating": 
         print("https://megankociscak.com/#/reevaluate-reevaluating/")
-        print("Which title would you like to see next?  ")
-        print("Type LIST to view available titles")
-        print("Type EXIT to exit")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
 
-elif new_choice == "Reevaluate Your Craft": 
+if new_choice == "Reevaluate Your Craft": 
         print("https://megankociscak.com/#/reevaluate-your-craft")
-        print("Which title would you like to see next?  ")
-        print("Type LIST to view available titles")
-        print("Type EXIT to exit")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
 
-elif new_choice == "EXIT":
-        sys.exit("Goodbye") 
-        
-elif new_choice == "LIST":
-        print(my_list)      
+if new_choice == "Darlin', Your Head's Not Right": 
+        print("https://megankociscak.com/#/darlin-your-heads-not-right/")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
+
+if new_choice == "Oh Shun View: I Had to Make him Leave": 
+        print("https://megankociscak.com/#/oh-shun-view/")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
+
+if new_choice == "A Pansy-Ass Box Of Whoop-Ass": 
+        print("https://megankociscak.com/#/a-pansy-ass-box-of-whoop-ass/")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
+
+if new_choice == "One Day, Baby, We'll Be Gold": 
+        print("https://megankociscak.com/#/one-day-baby-well-be-gold/")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
+
+if new_choice == "A Parking Lot Made Up Of Makeup (Another Waste Of Space)": 
+        print("https://megankociscak.com/#/a-parking-lot-made-up-of-makeup/")
+        print(show_directions())
+        new_choice = input("Which title would you like to see? (copy/paste from list)  ")
+
+if new_choice == "EXIT":
+        sys.exit("Goodbye")
+               
+if new_choice == "LIST":
+        print(my_list)       
+
 else:
-        input("Please choose from the list above  ")
+        print("Please choose from the list above  ")
     
+
+new_choice = input("Which title would you like to see? (copy/paste from list)  ")
